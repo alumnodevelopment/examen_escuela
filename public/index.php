7e48297cd26d283596c82b22f55536f14db3f4c6
marino5710 <brianmarin4@gmail.com>
2023-09-29 14:06:01 -0600

    se modificaron las rutas

--- public/index.php
+++ public/index.php
@@ -5,6 +5,8 @@ require_once __DIR__ . '/../includes/app.php';
 use MVC\Router;
 use Controllers\AppController;
 use Controllers\GradoController;
+use Controllers\AlumnoController;
+use Controllers\TutorController;
 
 $router = new Router();
 $router->setBaseURL('/' . $_ENV['APP_NAME']);
