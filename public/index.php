63faccf4b7004511657088c5114f8c03ae215620
marino5710 <brianmarin4@gmail.com>
2023-09-29 13:59:18 -0600

    se agregaron las rutas para hacer pruebas

--- public/index.php
+++ public/index.php
@@ -12,6 +12,27 @@ $router->setBaseURL('/' . $_ENV['APP_NAME']);
 $router->get('/', [AppController::class,'index']);
 
 
+$router->get('/alumnos', [AlumnoController::class, 'index']);
+$router->get('/API/alumnos/buscar', [AlumnoController::class, 'buscarAPI']);
+$router->post('/API/alumnos/guardar', [AlumnoController::class, 'guardarAPI']);
+$router->post('/API/alumnos/modificar', [AlumnoController::class, 'modificarAPI']);
+$router->post('/API/alumnos/eliminar', [AlumnoController::class, 'eliminarAPI']);
+
+//tutores
+$router->get('/tutores', [TutorController::class,'index']);
+$router->post('/API/tutores/guardar', [TutorController::class,'guardarAPI'] );
+$router->post('/API/tutores/modificar', [TutorController::class,'modificarAPI'] );
+$router->post('/API/tutores/eliminar', [TutorController::class,'eliminarAPI'] );
+$router->get('/API/tutores/buscar', [TutorController::class,'buscarAPI'] );
+
+
+//profesores
+$router->get('/profesores', [ProfesorController::class,'index']);
+$router->post('/API/profesores/guardar', [ProfesorController::class,'guardarAPI'] );
+$router->post('/API/profesores/modificar', [ProfesorController::class,'modificarAPI'] );
+$router->post('/API/profesores/eliminar', [ProfesorController::class,'eliminarAPI'] );
+$router->get('/API/profesores/buscar', [ProfesorController::class,'buscarAPI'] );
+
 $router->get('/grados/datatable', [GradoController::class,'datatable']);
 $router->post('/API/grados/guardar', [GradoController::class,'guardarAPI'] );
 $router->post('/API/grados/modificar', [GradoController::class,'modificarAPI'] );
