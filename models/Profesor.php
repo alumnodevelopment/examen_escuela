34c8cc7bc78b2902536ccaa76318c3392da8638b
marino5710 <brianmarin4@gmail.com>
2023-09-29 07:04:34 -0600

    se creo el modelo profesor para registrar los datos de los profesores

---
+++ models/Profesor.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Model;
+
+class Profesor extends ActiveRecord
+{
+    protected static $tabla = 'profesores';
+    protected static $columnasDB = ['profesor_nombre', 'profesor_telefono', 'profesor_situacion'];
+    protected static $idTabla = 'profesor_id';
+
+    public $profesor_id;
+    public $profesor_nombre;
+    public $profesor_telefono;
+    public $profesor_situacion;
+
+    public function __construct($args = [])
+    {
+        $this->profesor_id = $args['profesor_id'] ?? null;
+        $this->profesor_nombre = $args['profesor_nombre'] ?? '';
+        $this->profesor_telefono = $args['profesor_telefono'] ?? '';
+        $this->profesor_situacion = $args['profesor_situacion'] ?? 1;
+    }
+}
