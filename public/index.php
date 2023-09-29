8b6892d95d31e8bcacf4c4c438a18213160a8e9f
marino5710 <brianmarin4@gmail.com>
2023-09-29 07:31:47 -0600

    se agregaron las rutas de buscar, eliminar, modificar, guardar de profesores

--- public/index.php
+++ public/index.php
@@ -22,6 +22,10 @@ $router->get('/API/tutores/buscar', [TutorController::class,'buscarAPI'] );
 
 //profesores
 $router->get('/profesores', [ProfesorController::class,'index']);
+$router->post('/API/profesores/guardar', [ProfesorController::class,'guardarAPI'] );
+$router->post('/API/profesores/modificar', [ProfesorController::class,'modificarAPI'] );
+$router->post('/API/profesores/eliminar', [ProfesorController::class,'eliminarAPI'] );
+$router->get('/API/profesores/buscar', [ProfesorController::class,'buscarAPI'] );
 
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
