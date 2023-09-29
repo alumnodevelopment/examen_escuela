7578fd38ca0a5c163ff0e346cb217352744095c7
marino5710 <brianmarin4@gmail.com>
2023-09-29 07:07:57 -0600

    se creo el controlador de profesorController y se agrego la ruta al archivo index public

---
+++ controllers/ProfesorController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Controllers;
+
+use Exception;
+use Model\Profesor;
+use MVC\Router;
+
+
+class ProfesorController {
+    public static function index(Router $router)
+    {
+        $router->render('profesor/index', []);
+      
+}
+
+}
--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/../includes/app.php';
 use MVC\Router;
 use Controllers\AppController;
 use Controllers\TutorController;
+use Controllers\ProfesorController;
 
 $router = new Router();
 $router->setBaseURL('/' . $_ENV['APP_NAME']);
@@ -17,6 +18,10 @@ $router->post('/API/tutores/guardar', [TutorController::class,'guardarAPI'] );
 $router->post('/API/tutores/modificar', [TutorController::class,'modificarAPI'] );
 $router->post('/API/tutores/eliminar', [TutorController::class,'eliminarAPI'] );
 $router->get('/API/tutores/buscar', [TutorController::class,'buscarAPI'] );
+
+
+//profesores
+$router->get('/profesores', [ProfesorController::class,'index']);
 
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
