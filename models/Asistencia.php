e6711fa256eb27a185c22827d934c5bc62d3689e
marino5710 <brianmarin4@gmail.com>
2023-09-30 13:30:48 -0600

    se creo el modelo de asistencia

--- models/Asistencia.php
+++ models/Asistencia.php
@@ -2,20 +2,23 @@
 
 namespace Model;
 
-class Asistencia extends ActiveRecord{
+class Asistencia extends ActiveRecord {
     protected static $tabla = 'asistencia';
-    protected static $columnasDB = ['alumno_id','asistencia_fecha','asistencia_asistio', 'asistencia_situacion'];
+    protected static $columnasDB = ['grado_id', 'seccion_id', 'alumno_id', 'asistencia_fecha', 'asistencia_asistio', 'asistencia_situacion'];
     protected static $idTabla = 'asistencia_id';
     
     public $asistencia_id;
+    public $grado_id;
+    public $seccion_id;
     public $alumno_id;
     public $asistencia_fecha;
     public $asistencia_asistio;
     public $asistencia_situacion;
 
-    public function __construct($args = [])
-    {
+    public function __construct($args = []) {
         $this->asistencia_id = $args['asistencia_id'] ?? null;
+        $this->grado_id = $args['grado_id'] ?? '';
+        $this->seccion_id = $args['seccion_id'] ?? '';
         $this->alumno_id = $args['alumno_id'] ?? '';
         $this->asistencia_fecha = $args['asistencia_fecha'] ?? '';
         $this->asistencia_asistio = $args['asistencia_asistio'] ?? '';
