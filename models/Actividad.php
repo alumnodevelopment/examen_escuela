43d1150530b1fb046981c71da33ebc3e9a96285d
sergio-br0 <rodriguezbolvito4@gmail.com>
2023-09-30 19:56:02 -0600

    correcciones en el model actividades

--- models/Actividad.php
+++ models/Actividad.php
@@ -4,11 +4,13 @@ namespace Model;
 
 class Actividad extends ActiveRecord {
     protected static $tabla = 'actividades'; 
-    protected static $columnasDB = ['profesor_id', 'fecha_inicio', 'fecha_fin', 'actividad_descripcion', 'actividad_situacion'];
+    protected static $columnasDB = ['profesor_id', 'grado_id', 'seccion_id', 'fecha_inicio', 'fecha_fin', 'actividad_descripcion', 'actividad_situacion'];
     protected static $idTabla = 'actividad_id';
 
     public $actividad_id;
     public $profesor_id;
+    public $grado_id;
+    public $seccion_id;
     public $fecha_inicio;
     public $fecha_fin;
     public $actividad_descripcion;
@@ -17,6 +19,8 @@ class Actividad extends ActiveRecord {
     public function __construct($args = []) {
         $this->actividad_id = $args['actividad_id'] ?? null;
         $this->profesor_id = $args['profesor_id'] ?? '';
+        $this->grado_id = $args['grado_id'] ?? '';
+        $this->seccion_id = $args['seccion_id'] ?? '';
         $this->fecha_inicio = $args['fecha_inicio'] ?? '';
         $this->fecha_fin = $args['fecha_fin'] ?? '';
         $this->actividad_descripcion = $args['actividad_descripcion'] ?? '';
