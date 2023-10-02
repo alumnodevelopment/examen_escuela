dc977858f61ed5b196b9d6df9e83d5f55cea7d69
Livni <llmc0383@gmail.com>
2023-10-01 21:01:43 -0600

    agregar controller de reporte pago al index enrutador

--- public/index.php
+++ public/index.php
@@ -10,6 +10,7 @@ use Controllers\AlumnoController;
 use Controllers\AsistenciaController;
 use Controllers\ProfesorController;
 use Controllers\PagoController;
+use Controllers\ReportePagoController;
 
 
 $router = new Router();
@@ -17,6 +18,9 @@ $router->setBaseURL('/' . $_ENV['APP_NAME']);
 
 $router->get('/', [AppController::class,'index']);
 
+
+$router->get('/API/reportePagos/generar', [ReportePagoController::class, 'pdf']);
+
 $router->get('/alumnos', [AlumnoController::class, 'index']);
 $router->get('/API/alumnos/buscar', [AlumnoController::class, 'buscarAPI']);
 $router->post('/API/alumnos/guardar', [AlumnoController::class, 'guardarAPI']);
@@ -24,11 +28,7 @@ $router->post('/API/alumnos/modificar', [AlumnoController::class, 'modificarAPI'
 $router->post('/API/alumnos/eliminar', [AlumnoController::class, 'eliminarAPI']);
 
 
-$router->get('/alumnos', [AlumnoController::class, 'index']);
-$router->get('/API/alumnos/buscar', [AlumnoController::class, 'buscarAPI']);
-$router->post('/API/alumnos/guardar', [AlumnoController::class, 'guardarAPI']);
-$router->post('/API/alumnos/modificar', [AlumnoController::class, 'modificarAPI']);
-$router->post('/API/alumnos/eliminar', [AlumnoController::class, 'eliminarAPI']);
+
 
 //tutores
 $router->get('/tutores', [TutorController::class,'index']);
